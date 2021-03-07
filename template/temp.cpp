6684d8c292e212688aae94f3b6c29f8d9e358b37
HarukiTakamura <harki20452036@gmail.com>
2021-03-07 10:53:40 +0900

    Add template

---
+++ template/temp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<map>
+
+#define ALL(x) (x).begin(), (x)end()
+#define FOR(i,begin,end) for(int i=(begin);i<(end);i++)
+
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+
+}
